3d7e06bbb0ebd14e831192c4e77c21a105dd86b2
Huy <giahuychau07@gmail.com>
2025-11-28 02:01:11 -0500

    part1b 1.3 submission

--- Part 1/part1/src/App.jsx
+++ Part 1/part1/src/App.jsx
@@ -1,7 +1,8 @@
 const Header = (props) => {
+  console.log(props)
   return (
     <>
-      <p>Course name: {props.course}</p>
+      <h1>Course name: {props.course}</h1>
     </>
   )
   
@@ -29,19 +30,25 @@ const Total = () => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Content = () => {
     return(
       <>
-        <Part part={part1} exercises={exercises1}/>
-        <Part part={part2} exercises={exercises2}/>
-        <Part part={part3} exercises={exercises3}/>
+        <Part part={part1.name} exercises={part1['exercises']}/>
+        <Part part={part2.name} exercises={part2['exercises']}/>
+        <Part part={part3.name} exercises={part3['exercises']}/>
       </>
     )
   }
