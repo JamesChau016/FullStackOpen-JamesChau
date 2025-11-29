2caf1855d2ca81b7d1d1d2ccffab83f3f6c6029b
Huy <giahuychau07@gmail.com>
2025-11-29 16:44:51 -0500

    1.8

--- Part 1/Unicafe/src/App.jsx
+++ Part 1/Unicafe/src/App.jsx
@@ -1,29 +1,47 @@
 import { useState } from 'react'
 
+
+
+const Statistics = ({good, bad, neutral}) => {
+  const all = good+bad+neutral
+  const avr = (good-bad)/all
+  const pos = (good/all)*100
+  return(
+    <>
+      <h2>Statistics</h2>
+      <p>good {good} <br/>
+         neutral {neutral} <br/>
+         bad {bad}<br/>
+         all {all}<br/>
+         average {avr}<br/>
+         positive {pos} %
+      </p>
+
+    </>
+  )
+}
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const avr = (good-bad)/all
-  const pos = (good/all)*100
+  // const [all, setAll] = useState(0)
 
   const handleGood = () =>{
     const newGood = good+1
     setGood(newGood)
-    setAll(newGood+bad+neutral)
+    // setAll(newGood+bad+neutral)
   }
 
   const handleNeutral = () =>{
     const newNeu = neutral+1
     setNeutral(newNeu)
-    setAll(good+bad+newNeu)
+    // setAll(good+bad+newNeu)
   }
   const handleBad = () =>{
     const newBad = bad+1
     setBad(newBad)
-    setAll(good+newBad+neutral)
+    // setAll(good+newBad+neutral)
   }
 
 
@@ -35,14 +53,7 @@ const App = () => {
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
-      <h2>statistics</h2>
-      <p>good {good} <br/>
-         neutral {neutral} <br/>
-         bad {bad}<br/>
-         all {all}<br/>
-         average {avr}<br/>
-         positive {pos} %
-      </p>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
