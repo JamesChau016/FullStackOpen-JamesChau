27c16b2e3ca5430b03b7519fb01f048f22018cdd
Huy <giahuychau07@gmail.com>
2025-11-29 16:49:42 -0500

    1.9

--- Part 1/Unicafe/src/App.jsx
+++ Part 1/Unicafe/src/App.jsx
@@ -6,45 +6,46 @@ const Statistics = ({good, bad, neutral}) => {
   const all = good+bad+neutral
   const avr = (good-bad)/all
   const pos = (good/all)*100
-  return(
-    <>
-      <h2>Statistics</h2>
-      <p>good {good} <br/>
-         neutral {neutral} <br/>
-         bad {bad}<br/>
-         all {all}<br/>
-         average {avr}<br/>
-         positive {pos} %
-      </p>
-
-    </>
-  )
+  if (all===0){
+    return(
+      <p>No feedback given</p>
+    )
+  }
+  else{
+    return(
+      <>
+        <h2>Statistics</h2>
+        <p>good {good} <br/>
+           neutral {neutral} <br/>
+           bad {bad}<br/>
+           all {all}<br/>
+           average {avr}<br/>
+           positive {pos} %
+        </p>
+  
+      </>
+    )
+  }
 }
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  // const [all, setAll] = useState(0)
 
   const handleGood = () =>{
     const newGood = good+1
     setGood(newGood)
-    // setAll(newGood+bad+neutral)
   }
 
   const handleNeutral = () =>{
     const newNeu = neutral+1
     setNeutral(newNeu)
-    // setAll(good+bad+newNeu)
   }
   const handleBad = () =>{
     const newBad = bad+1
     setBad(newBad)
-    // setAll(good+newBad+neutral)
   }
-
-
 
 
   return (
@@ -54,6 +55,7 @@ const App = () => {
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
       <Statistics good={good} bad={bad} neutral={neutral}/>
+      
     </div>
   )
 }
