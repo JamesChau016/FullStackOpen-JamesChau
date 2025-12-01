408974303d49a884d7b1ca42f9b9779012357e9d
Huy <giahuychau07@gmail.com>
2025-11-30 23:35:27 -0500

    2.1 fix:  work regardless of the number of parts a course has

--- Part 1/CourseInfo/src/components/course.jsx
+++ Part 1/CourseInfo/src/components/course.jsx
@@ -1,12 +1,12 @@
 const Header = (props) => <h1>{props.course}</h1>
 
-const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
+const Content = (props) => {
+    
+    const arr = props.parts
+    return(
+        arr.map((part,i) => <Part key={i} part={part}/>)
+    )
+}
 
 const Part = (props) => (
   <p>
