e0dfba15ddca59e498fa99ac30af5b6895272a3d
Huy <giahuychau07@gmail.com>
2025-12-01 21:16:05 -0500

    2.8

--- Part 2/ThePhoneBook/src/App.jsx
+++ Part 2/ThePhoneBook/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567'
+     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
 
 
   const addName = (e) =>{
-    const obj={name:newName}
+    const obj={name:newName, number: newNum}
     e.preventDefault()
     if (persons.find(o=>o.name===newName)){
       window.alert(`${newName} is already added to the phonebook`)
@@ -19,8 +22,12 @@ const App = () => {
     
   }
 
-  const handleChange = (e) =>{
+  const handleChangeName = (e) =>{
     setNewName(e.target.value)
+  }
+
+  const handleChangeNum = (e) =>{
+    setNewNum(e.target.value)
   }
 
   return (
@@ -28,7 +35,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input onChange={handleChange}/>
+          name: <input onChange={handleChangeName}/>
+        </div>
+        <div>
+          number: <input onChange={handleChangeNum}/>
         </div>
         <div>
           <button type="submit" >add</button>
@@ -36,7 +46,7 @@ const App = () => {
         {/* <div>debug: {newName}</div> */}
       </form>
       <h2>Numbers</h2>
-      {persons.map((n,i)=><div key={i}>{n.name}</div>)}
+      {persons.map((n,i)=><div key={i}>{n.name} {n.number}</div>)}
     </div>
   )
 }
