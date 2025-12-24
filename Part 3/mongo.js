066d1709211ad8ebff668e4612669a678806e426
Huy <giahuychau07@gmail.com>
2025-12-24 01:26:27 -0500

    3.12

---
+++ Part 3/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://ghuy_fullstack:${password}@cluster0.ci7s1sw.mongodb.net/PhoneBook?appName=Cluster0`
+
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = mongoose.Schema({
+    "name" : String,
+    "number" : String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+
+
+if (process.argv[3]){
+    const person = new Person(
+        { 
+            "name" : process.argv[3], 
+            "number": process.argv[4]
+        }
+    )
+    person.save().then(result =>{
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+else {
+    Person.find({}).then(result =>{
+        console.log('phonebook:')
+        result.forEach(p=>console.log(p.name, p.number))
+        mongoose.connection.close()
+    })
+}
+
+
+
