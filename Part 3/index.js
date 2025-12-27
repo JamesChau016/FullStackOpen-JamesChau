12ca5b05be126ab7bd8f9e876de7dcd84304f948
Huy <giahuychau07@gmail.com>
2025-12-26 23:05:41 -0500

    3.17

--- Part 3/index.js
+++ Part 3/index.js
@@ -87,7 +87,6 @@ app.delete('/api/persons/:id', (request,response,next)=>{
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-  
 
 app.use(unknownEndpoint)
 
