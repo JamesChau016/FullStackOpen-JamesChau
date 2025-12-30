526aba84d06e0da146805fadcc35b4ce04feafc6
Huy <giahuychau07@gmail.com>
2025-12-30 01:41:00 -0500

    middleware refactor

--- Part 4/index.js
+++ Part 4/index.js
@@ -2,11 +2,6 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-
-
-
-
-
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
   logger.info(`http://localhost:3003/api/blogs`)
