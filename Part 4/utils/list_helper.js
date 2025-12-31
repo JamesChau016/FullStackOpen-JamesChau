239878a17bd0e8249f8f1110fc61c3ade0d47542
Huy <giahuychau07@gmail.com>
2025-12-31 00:06:24 -0500

    totalLikes test

--- Part 4/utils/list_helper.js
+++ Part 4/utils/list_helper.js
@@ -1,7 +1,21 @@
 const dummy = (blogs) => {
     return 1
   }
-  
-  module.exports = {
-    dummy
+
+const totalLikes = (blogs) => {
+  if (blogs.length == 1){
+    return blogs[0].likes
   }
+  else{
+    return blogs.reduce((accumulator, curr)=>{ 
+      return accumulator + curr.likes
+    },0)
+  }
+  
+}
+
+  module.exports = {
+    dummy,
+    totalLikes
+  }
+
