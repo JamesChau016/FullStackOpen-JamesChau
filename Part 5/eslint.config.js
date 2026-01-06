9f84d8ac76d229bc2c93606dca92a6bc406bfa0c
Huy <giahuychau07@gmail.com>
2026-01-05 22:50:33 -0500

    eslint config

--- Part 5/eslint.config.js
+++ Part 5/eslint.config.js
@@ -21,14 +21,14 @@ export default [
       'react-refresh': reactRefresh
     },
     rules: {
-      "no-unused-var" : "off",
+      'no-console': 'off',
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true }
-      ]
+      ],
+      "no-unused-vars": "off"
     }
   }
 ]
