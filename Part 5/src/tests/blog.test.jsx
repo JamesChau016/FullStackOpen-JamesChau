6908880092ac5611b395c7804cbd918763922421
Huy <giahuychau07@gmail.com>
2026-01-09 00:34:26 -0500

    5.14

--- Part 5/src/tests/blog.test.jsx
+++ Part 5/src/tests/blog.test.jsx
@@ -1,21 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import Blog from '../components/Blog.jsx'
+import userEvent from '@testing-library/user-event'
 
+const blogs = [
+  {
+    'title': 'blog rendering test',
+    'author': 'Jammie',
+    'url': 'abc.com',
+    'likes': 7
+  },
+]
+
+const user = {
+  'username': 'Jimmy',
+  'name': 'James'
+}
 
 test('renders content', () => {
-  const blogs = [
-    {
-      'title': 'blog rendering test',
-      'author': 'Jammie',
-      'url': 'abc.com',
-      'likes': 7
-    },
-  ]
-
-  const user = {
-    'username': 'Jimmy',
-    'name': 'James'
-  }
 
   render(<Blog blogs={blogs} user={user}/>)
 
@@ -29,4 +30,19 @@ test('renders content', () => {
   expect(author).toBeNull()
   expect(url).toBeNull()
   expect(likes).toBeNull()
+})
+
+test('view details button', async () => {
+
+  render(<Blog blogs={blogs} user={user}/>)
+
+  const mockUser = userEvent.setup()
+  const button = screen.getByText('view')
+  await mockUser.click(button)
+
+  const url = screen.queryByText('url')
+  const likes = screen.queryByText('likes')
+
+  expect(url).toBeDefined()
+  expect(likes).toBeDefined()
 })
