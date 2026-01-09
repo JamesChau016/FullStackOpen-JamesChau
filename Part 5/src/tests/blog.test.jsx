c418a85e8eba99c69f519dfb5bc6c9f684b6b0c9
Huy <giahuychau07@gmail.com>
2026-01-09 00:59:48 -0500

    5.16

--- Part 5/src/tests/blog.test.jsx
+++ Part 5/src/tests/blog.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
-import Blog from '../components/Blog.jsx'
 import userEvent from '@testing-library/user-event'
+import Blog from '../components/Blog'
+import BlogForm from '../components/BlogForm'
+
 
 const blogs = [
   {
@@ -48,13 +50,35 @@ test('view details button', async () => {
 })
 
 test('test like button', async () => {
-  const mockHandler = vi.fn()
-  render(<Blog blogs={blogs} user={user} handleLike={mockHandler}/>)
+  const likeHandler = vi.fn()
+  render(<Blog blogs={blogs} user={user} handleLike={likeHandler}/>)
 
   const mockUser = userEvent.setup()
   const button = screen.getByText('like')
   await mockUser.click(button)
   await mockUser.click(button)
 
-  expect(mockHandler.mock.calls).toHaveLength(2)
+  expect(likeHandler.mock.calls).toHaveLength(2)
+})
+
+test('blog form test', async () => {
+  const createBlog = vi.fn()
+  const mockUser = userEvent.setup()
+
+  render(<BlogForm handleCreate={createBlog}/>)
+
+    const titleInput = screen.getByLabelText('title:',{ exact: false })
+    const authorInput = screen.getByLabelText('author:',{ exact: false })
+    const urlInput = screen.getByLabelText('url:',{ exact: false })
+    const createBtn = screen.getByText('create')
+
+    await mockUser.type(titleInput, 'test form title input')
+    await mockUser.type(authorInput, 'test form author input')
+    await mockUser.type(urlInput, 'test form url input')
+    await mockUser.click(createBtn)
+
+    expect(createBlog.mock.calls).toHaveLength(1)
+    expect(createBlog.mock.calls[0][0].title).toBe('test form title input')
+    expect(createBlog.mock.calls[0][0].author).toBe('test form author input')
+    expect(createBlog.mock.calls[0][0].url).toBe('test form url input')
 })
