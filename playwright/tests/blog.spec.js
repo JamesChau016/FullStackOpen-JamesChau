7d005465c5fcf517d8577f2cbe60c76b8017eda2
Huy <giahuychau07@gmail.com>
2026-01-11 23:59:20 -0500

    5.18

--- playwright/tests/blog.spec.js
+++ playwright/tests/blog.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+        data:{
+            "name": "admin",
+            "username": "admin",
+            "password": "admin"
+        }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -9,5 +18,23 @@ describe('Blog app', () => {
     const locator = page.getByRole('heading', {name: 'Log in'})
     await expect(locator).toBeVisible()
     await expect(page.getByRole('button', {name: 'login'})).toBeVisible()
+  })
+
+  describe('login', () => {
+    test('login successful', async ({page}) => {
+        await page.getByLabel('username').fill('admin')
+        await page.getByLabel('password').fill('admin')
+        await page.getByRole('button', {name: 'login'}).click()
+
+        await expect(page.getByText('admin logged in')).toBeVisible()
+    })
+
+    test('login failed', async ({page}) => {
+        await page.getByLabel('username').fill('admin')
+        await page.getByLabel('password').fill('wrong')
+        await page.getByRole('button', {name: 'login'}).click()
+
+        await expect(page.getByRole('button', {name: 'log out'})).not.toBeVisible()
+    })
   })
 })
