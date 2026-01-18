405404b6c5ef669ca42e0f2331967945fb7a8791
Huy <giahuychau07@gmail.com>
2026-01-18 18:47:00 -0500

    fix unnecessary renders warning

--- Part 6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Part 6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,17 +1,24 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { createSelector } from '@reduxjs/toolkit'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
     const dispatch = useDispatch()
-    const anecdotes = useSelector(state => {
-        const sorted = [...state.anecdotes]
-        if (state.filter==='ALL'){
-            return sorted.sort((a,b) => b.votes-a.votes)
+    
+    const anecList = state => state.anecdotes
+    const filt = state => state.filter
+    const anecdotesCreator = createSelector(anecList, filt, (anecdotes, filter) => {
+        const sorted = [...anecdotes]
+        sorted.sort((a,b) => b.votes-a.votes)
+        if (filter==='ALL'){
+            return sorted
         }
         else{
-            return sorted.filter(s => s.content.includes(state.filter)).sort((a,b) => b.votes-a.votes)
+            return sorted.filter(s => s.content.includes(filter))
         }
     })
+    
+    const anecdotes = useSelector(anecdotesCreator)
 
     const handleVote = (id) => {
         dispatch(vote(id))
