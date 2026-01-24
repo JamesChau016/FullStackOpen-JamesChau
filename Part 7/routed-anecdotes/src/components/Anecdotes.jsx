1366c0c6cf96eec7453a19a69112e99c30c62498
Huy <giahuychau07@gmail.com>
2026-01-24 01:23:56 -0500

    refactor

---
+++ Part 7/routed-anecdotes/src/components/Anecdotes.jsx
@@ -0,0 +1,28 @@
+import { Link, useParams } from 'react-router-dom'
+
+export const AnecdoteList = ({ anecdotes }) => {
+    return(
+    <div>
+      <h2>Anecdotes</h2>
+      <ul>
+        {anecdotes.map(anecdote => <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>
+            {anecdote.content}
+          </Link>
+        </li>)}
+      </ul>
+    </div>
+)}
+
+export const Anecdote = ({ anecdotes }) => {
+    const id = useParams().id
+    const anecdote= anecdotes.find(a => a.id === Number(id))
+    
+    return(
+      <div>
+        <h2>{anecdote.content}</h2>
+        <p>has {anecdote.votes} votes</p>
+        <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+      </div>
+    )
+  }
